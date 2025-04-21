4b8478120db4f1ba0913b28df27d1d51429123c8
Divya Singh <divyafiitjee12@gmail.com>
2025-04-21 17:12:42 +0530

    built the user card

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -3,13 +3,14 @@ import { base_url } from '../utils/constant';
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import { addFeed } from '../utils/feedSlice';
+import UserCard from './UserCard';
 
 const Feed = () => {
   const dispatch = useDispatch(); 
  const Feed = useSelector((store) => store.feed);
    const getFeed = async () => {
      try {
-      if(Feed) return;
+      if(Feed && Feed.length>0) return;
       const res = await axios.get(base_url + "/feed", {withCredentials: true});
       dispatch(addFeed(res?.data));
      }catch (err) {
@@ -21,9 +22,17 @@ const Feed = () => {
      getFeed();
    }, []);
 
-  return (
-    <div>Feed</div>
-  )
+  return (Feed && (
+    <div className='flex flex-wrap justify-center gap-4 mt-4'> 
+      <UserCard user={Feed[2]}/>
+    </div>
+
+    // <div className="flex flex-wrap justify-center gap-4">
+    //   {Feed.map((feed) => (
+    //     <UserCard key={feed._id} feed={feed} />
+    //   ))}
+    // </div>
+  ));
 }
 
 export default Feed
