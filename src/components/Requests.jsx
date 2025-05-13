a8cb178ad8462f946fbe601c52ff82a1a1ab04db
Divya Singh <divyafiitjee12@gmail.com>
2025-05-13 16:40:17 +0530

    accepted/rejected connections request

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -2,11 +2,24 @@ import React, { useEffect } from 'react'
 import { base_url } from '../utils/constant';
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
-import { addRequests } from '../utils/requestsSlice';
+import { addRequests, removeRequest } from '../utils/requestsSlice';
 
 const Requests = () => {
     const dispatch = useDispatch();
     const requests = useSelector((store)=> store.requests);
+
+    const reviewRequest = async (status, _id) => {
+      try {
+        const res = await axios.post(
+          base_url + "/request/review" + "/" + status + "/" + _id,
+          {},
+          { withCredentials: true }
+        );
+        dispatch(removeRequest(_id));
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
     const fetchRequests = async()=>{
         try{
@@ -19,9 +32,7 @@ const Requests = () => {
         }catch(err){
             console.log(err?.response.data);
             
-        }
-
-     
+        } 
     }
     useEffect(()=>{
         fetchRequests();
@@ -40,13 +51,11 @@ const Requests = () => {
         <h1 className='text-bold text-white text-3xl items-center'>Connection Requests</h1>
         {requests && requests.map((request) => {
             const { _id, firstName, lastName, photoUrl, age, gender, about } =
-              request.fromUserId;
-              
-              
+              request.fromUserId; 
             return (
               <div
                 key={_id}
-                className="flex justify-between  items-center m-4 p-4 bg-base-300 rounded-lg shadow-sm my-4  w-1/3 mx-auto"    
+                className="flex justify-between  items-center m-4 p-4 bg-base-300 rounded-lg shadow-sm my-4  w-1/3 mx-auto"
               >
                 <div>
                   <img
@@ -57,14 +66,22 @@ const Requests = () => {
                 </div>
                 <div className="text-left mx-4">
                   <h2 className="text-xl">{firstName + " " + lastName}</h2>
-                   {age && gender && <p>{age + " " + gender}</p>}
-                    {about && <p>{about}</p>}
-                    
+                  {age && gender && <p>{age + " " + gender}</p>}
+                  {about && <p>{about}</p>}
                 </div>
-
                 <div className="card-actions justify-end  ">
-                    <button className="btn btn-primary mx-2">Accept</button>
-                    <button className="btn btn-secondary">Reject</button>
+                  <button
+                    className="btn btn-primary mx-2"
+                    onClick={() => reviewRequest("accepted", request._id)}
+                  >
+                    Accept
+                  </button>
+                  <button
+                    className="btn btn-secondary"
+                    onClick={() => reviewRequest("rejected", request._id)}
+                  >
+                    Reject
+                  </button>
                 </div>
               </div>
             );
